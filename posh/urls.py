bef0191b73ddc0d11ea3c0f3a03c393d3a691994
amitm15 <amitmaity0415@gmail.com>
2024-07-02 22:05:13 +0530

    reset-password-functionality added

--- posh/urls.py
+++ posh/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name="home" ),
@@ -78,6 +79,21 @@ urlpatterns = [
     path('recruit-user/<str:pk>/', views.recruit, name="recruit"),
     path('all-invites/', views.all_invites, name="all_invites"),
     path('all-establishment/', views.all_establishment, name="all_establishment"),
+
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), 
+        name='password_change_done'),
+
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'), 
+        name='password_change'),
+
+    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
+     name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+     name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
