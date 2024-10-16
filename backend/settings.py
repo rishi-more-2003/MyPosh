14ad4d07e5942b41ae86df4cb0479cc9f956617e
amitm15 <amitmaity0415@gmail.com>
2024-10-16 19:34:14 +0530

    changes

--- backend/settings.py
+++ backend/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
