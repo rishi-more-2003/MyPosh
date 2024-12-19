f9b717ad33366129e66e66ccdb06b945a393c135
amitm15 <amitmaity0415@gmail.com>
2024-12-19 20:08:50 +0530

    update

--- build.sh
+++ build.sh
@@ -7,3 +7,6 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
+
+# Start the Gunicorn server with a custom timeout
+gunicorn backend.wsgi:application --bind 0.0.0.0:8000 --timeout 120
