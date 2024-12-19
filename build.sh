da46346a496744cf07258efda49f2351e7d7a736
amitm15 <amitmaity0415@gmail.com>
2024-12-19 21:10:03 +0530

    updated timeout

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ python manage.py makemigrations
 python manage.py migrate
 
 # Start the Gunicorn server with a custom timeout
-gunicorn backend.wsgi:application --bind 0.0.0.0:8000 --timeout 120
+gunicorn backend.wsgi:application --timeout 120
