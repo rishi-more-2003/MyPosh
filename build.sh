0049994fc6cdbf3d504aa0c23b09f88ff5cbf48c
amitm15 <amitmaity0415@gmail.com>
2024-12-21 00:10:43 +0530

    update

--- build.sh
+++ build.sh
@@ -7,5 +7,3 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-
-gunicorn -w 2 -b 0.0.0.0:5432 backend.wsgi:application --timeout 120 
